a5a5ff021cad25fd5806915178a48766e74f6bbe
Pedro Lucas L. Costa <pedro.lucas.luz.dev@gmail.com>
2024-05-13 16:32:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# API-REST-sobre-os-campeoes-do-league-of-legends
+ RESTful API construída em Java 21 com Spring Boot 3
---
+++ src/main/java/me/dio/santandersdw24/SantanderDevWeek2024Application.java
@@ -0,0 +1,14 @@
+package me.dio.santandersdw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024Application.class, args);
+	}
+
+}
+
