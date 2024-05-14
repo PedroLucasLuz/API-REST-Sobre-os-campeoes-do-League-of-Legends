906581c2f83b40021ad0347e72d7d30c3d878c31
Pedro Lucas L. Costa <pedro.lucas.luz.dev@gmail.com>
2024-05-14 15:11:19 -0300

    :sparkles:feat: Desenvolvimento a nível de domínio

---
+++ src/main/java/me/dio/santandersdw24/domain/model/Champions.java
@@ -0,0 +1,11 @@
+package me.dio.santandersdw24.domain.model;
+
+public record Champions(
+        Long id,
+        String name,
+        String role,
+        String lore,
+        String imageUrl
+) {
+
+}
---
+++ src/main/java/me/dio/santandersdw24/domain/ports/ChampionsRepository.java
@@ -0,0 +1,14 @@
+package me.dio.santandersdw24.domain.ports;
+
+import me.dio.santandersdw24.domain.model.Champions;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChampionsRepository {
+
+    List<Champions> findAll();
+
+    Optional<Champions> findById(Long id);
+
+}
